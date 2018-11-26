99c2b06f1a6680518f5e91b0f0a56288884395fe
Adam Able <k1763909@kcl.ac.uk>
2018-11-26 12:03:29 +0000

    Make signup system and finish login

--- database_setup.sql
+++ database_setup.sql
@@ -1,11 +1,12 @@
 CREATE DATABASE games_database;
 USE games_database;
 
-CREATE TABLE Member (
-    id INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE members (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     email VARCHAR(255) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL    
+    password VARCHAR(255) NOT NULL,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP
 );
 
-INSERT INTO Member (email, password) VALUES ('adam.able@kcl.ac.uk', '123456789');
-INSERT INTO Member (email, password) VALUES ('k1763909@kcl.ac.uk', '987654321');
+/*INSERT INTO members (email, password) VALUES ('adam.able@kcl.ac.uk', '123456789');
+INSERT INTO members (email, password) VALUES ('k1763909@kcl.ac.uk', '987654321');*/
--- game_rental/public/sign-up.php
+++ game_rental/public/sign-up.php
@@ -1,4 +1,100 @@
+
 <?php
+// Include config file
+require_once "../private/initialize.php";
+ 
+// Define variables and initialize with empty values
+$email = $password = $confirm_password = "";
+$email_err = $password_err = $confirm_password_err = "";
+ 
+// Processing form data when form is submitted
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+ 
+    // Validate username
+    if(empty(trim($_POST["email"]))){
+        $email_err = "Please enter an email.";
+    } else{
+        // Prepare a select statement
+        $sql = "SELECT id FROM members WHERE email = ?";
+        
+        if($stmt = mysqli_prepare($db, $sql)){
+            // Bind variables to the prepared statement as parameters
+            mysqli_stmt_bind_param($stmt, "s", $param_email);
+            
+            // Set parameters
+            $param_email = trim($_POST["email"]);
+            
+            // Attempt to execute the prepared statement
+            if(mysqli_stmt_execute($stmt)){
+                /* store result */
+                mysqli_stmt_store_result($stmt);
+                
+                if(mysqli_stmt_num_rows($stmt) == 1){
+                    $email_err = "This email is already in use.";
+                } else{
+                    $email = trim($_POST["email"]);
+                }
+            } else{
+                echo "Oops! Something went wrong. Please try again later.";
+            }
+        }
+         
+        // Close statement
+        mysqli_stmt_close($stmt);
+    }
+    
+    // Validate password
+    if(empty(trim($_POST["password"]))){
+        $password_err = "Please enter a password.";     
+    } elseif(strlen(trim($_POST["password"])) < 6){
+        $password_err = "Password must have atleast 6 characters.";
+    } else{
+        $password = trim($_POST["password"]);
+    }
+    
+    // Validate confirm password
+    if(empty(trim($_POST["confirm_password"]))){
+        $confirm_password_err = "Please confirm password.";     
+    } else{
+        $confirm_password = trim($_POST["confirm_password"]);
+        if(empty($password_err) && ($password != $confirm_password)){
+            $confirm_password_err = "Password did not match.";
+        }
+    }
+    
+    // Check input errors before inserting in database
+    if(empty($email_err) && empty($password_err) && empty($confirm_password_err)){
+        
+        // Prepare an insert statement
+        $sql = "INSERT INTO members (email, password) VALUES (?, ?)";
+         
+        if($stmt = mysqli_prepare($db, $sql)){
+            // Bind variables to the prepared statement as parameters
+            mysqli_stmt_bind_param($stmt, "ss", $param_email, $param_password);
+            
+            // Set parameters
+            $param_email = $email;
+            $param_password = password_hash($password, PASSWORD_DEFAULT); // Creates a password hash
+            
+            // Attempt to execute the prepared statement
+            if(mysqli_stmt_execute($stmt)){
+                // Redirect to login page
+                header("location: login.php");
+            } else{
+                echo "Something went wrong. Please try again later.";
+            }
+        }
+         
+        // Close statement
+        mysqli_stmt_close($stmt);
+    }
+    
+    // Close connection
+    mysqli_close($db);
+}
+?>
+
+
 <!DOCTYPE HTML>
 <html lang="en">
 <head>
@@ -6,16 +102,19 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-  <link rel="stylesheet" href="styles/stylesheet.css" type="text/css">
+  <link rel="stylesheet" href="stylesheets/stylesheet.css" type="text/css">
 </head>
 
 <body>
   <div class="container jumbotron">
 
     <div class="row">
+    
       <div class="col-4"></div>
+      
       <div class="col jumbotron">
-        <form>
+      
+        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
           <div class ="from-group">
             <label for="exampleInputName1"> First Name</label>
             <br>
@@ -28,28 +127,37 @@
           </div>
 
           <br>
-          <div class="form-group">
+          
+          <div class="form-group <?php echo (!empty($username_err)) ? 'has-error' : ''; ?>">
             <label for="exampleInputEmail1">Email address</label>
-            <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
+            <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email" name="email" value="<?php echo $username; ?>">
+            <span class="help-block"><?php echo $email_err; ?>
           </div>
-          <div class="form-group">
+          
+          <div lass="form-group <?php echo (!empty($password_err)) ? 'has-error' : ''; ?>">
             <label for="exampleInputPassword1">Password</label>
-            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" name="password" value="<?php echo $password; ?>">
+            <span class="help-block"><?php echo $password_err; ?>
           </div>
-          <div class="form-group">
-            <label for="exampleConfirmPassword1">Confirm password</label>
-            <input type="password" class="form-control" id="exampleConfirmPassword1" placeholder="Re-enter your password">
+          
+          <div class="form-group <?php echo (!empty($confirm_password_err)) ? 'has-error' : ''; ?>">
+            <label for="exampleConfirmPassword1" >Confirm password</label>
+            <input type="password" class="form-control" id="exampleConfirmPassword1" placeholder="Re-enter your password" name="confirm_password" value="<?php echo $confirm_password; ?>">
+            <span class="help-block"><?php echo $confirm_password_err; ?></span>
           </div>
 
           <br>
+          
           <div class="form-group">
             <label for="exampleInputCardHolderName1">Cardholder Name</label>
             <input type="cardName" class="form-control" id="exampleInputCardHolderName1" placeholder="Enter the name on the card">
           </div>
+          
           <div class="form-group">
             <label for="exampleInputCardNumber1">Card Number</label>
             <input type="number" class="form-control" maxlenght="16" id="exampleInputCardNumber1"  placeholder="The 16 digit card number">
           </div>
+          
           <div class="form-group">
             <label for="exampleInputExpiryDate1">Expiry Date</label>
             <input type="month" class="form-control" id="exampleInputExpiryDate1" placeholder="month">
@@ -57,7 +165,9 @@
 
           <button type="submit" class="btn btn-primary">Sign up</button>
         </form>
+        
       </div>
+      
       <div class="col-4"></div>
     </div>
 
