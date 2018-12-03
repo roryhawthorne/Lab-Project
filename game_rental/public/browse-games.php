92dceb42a0404a95e87b64741cd08026145295b6
Adam Able <h0p0h0p0@hotmail.com>
2018-12-03 11:27:44 +0000

    Add images

---
+++ game_rental/public/browse-games.php
@@ -0,0 +1,59 @@
+<?php
+
+?>
+
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+  <title>Game Page Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+  <link rel="stylesheet" href="stylesheets/stylesheet.css" type="text/css">
+</head>
+
+<body>
+
+  <div class="container">
+
+    <br/>
+    <br/>
+    <br/>
+
+    <div class="row">
+      <div class="col-md-3"></div>
+      <h1 class="col-md-6">Computer Game Society</h1>
+      <div class="col-md-3"></div>
+    </div>
+
+    <br/>
+    <br/>
+    <br/>
+
+    <div class="row">
+      <div class="col-md-4 px-0 game">
+        <a href="game-example.php"><img src="images/bfv.jpg" class="img-fluid"/></a>
+      </div>
+      <div class="col-md-4 px-0 game">
+        <a href="#"><img src="images/divinity2.jpg" class="img-fluid"/></a>
+      </div>
+      <div class="col-md-4 px-0 game">
+        <a href="#"><img src="images/hitman2.jpg" class="img-fluid"/></a>
+      </div>
+    </div>
+
+    <br/>
+    <br/>
+    <br/>
+
+    <div class="row">
+      <div class="col-md-4 px-0 game">
+        <a href="#"><img src="images/forza4.png" class="img-fluid"/></a>
+      </div>
+      <div class="col-md-4 px-0 game">
+        <a href="#"><img src="images/undertale.jpg" class="img-fluid"/></a>
+      </div>
+    </div>
+
+  </div>
+<?php include(SHARED_PATH . '/footer.php'); ?>
