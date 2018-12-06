e5d9886c2c3dda64b3b44ccd503f14e4614d34ee
“Adam <k1763909@kcl.ac.uk>
2018-12-06 18:36:00 +0000

    Add menu bar

--- game_rental/public/browse-games.php
+++ game_rental/public/browse-games.php
@@ -4,70 +4,84 @@
 <!DOCTYPE HTML>
 <html lang="en">
 <head>
-  <title>Game Page Example</title>
+  <title>Computer Games Rental</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link href="https://fonts.googleapis.com/css?family=Bungee" rel="stylesheet">
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-  <link rel="stylesheet" href="stylesheets/stylesheet.css" type="text/css">
+  <link rel="stylesheet" media="all" type="text/css" href="stylesheets/stylesheet.css" />
 </head>
 
 <body>
 
-  <div class="container">
-
-    <br/>
-    <br/>
-    <br/>
-
-    <section>
-        <div class="row">
-          <div class="col-md-3"></div>
-          <h1 class="col-md-6">Computer Game Society</h1>
-          <div class="col-md-3"></div>
-        </div>
-    </section>
-
-    <br/>
-    <br/>
-    <br/>
+    <!-- navbar -->
+    <div class="shadow-lg">
+    <nav class="navbar navbar-expand-md navbar-light bg-faded shadow-lg">
+      <a class="navbar-brand" href="javascript:void(0)">
+        <img src="images/logo-header.png" width="380" height="98">
+      </a>
+      <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
     
-    <section>
-        <div class="row">
-            <div class="col-md-12">
-                <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-                  <ol class="carousel-indicators">
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav" style="margin: auto;">
+          <li class="nav-item" >
+            <a class="nav-link" href="javascript:void(0)">XBOX ONE</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="javascript:void(0)">PS4</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="javascript:void(0)">SWITCH</a>
+          </li>
+        </ul>
+        <ul class="navbar-nav">
+          <span class="navbar-brand" href="javascript:void(0)">
+            <img src="images/header-right.jpg" width="380" height="98">
+          </span>
+        </ul>
+      </div>
+    </nav>
+    </div>
+
+    
+    <div class="row">
+        <div class="col-md-12">
+            <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+                <ol class="carousel-indicators">
                     <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
                     <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
                     <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-                  </ol>
-                  <div class="carousel-inner">
-                    <div class="carousel-item active">
-                      <img class="d-block w-100" src="images/carousel/bfv-carousel.jpg" alt="First slide">
-                    </div>
-                    <div class="carousel-item">
-                      <img class="d-block w-100" src="images/carousel/shadowofthetombraider-carousel.jpg" alt="Second slide">
-                    </div>
-                    <div class="carousel-item">
-                      <img class="d-block w-100" src="images/carousel/divinity2-carousel.jpg" alt="Third slide">
-                    </div>
-                  </div>
-                  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                </ol>
+                <div class="carousel-inner">
+                      <div class="carousel-item active">
+                          <img class="d-block w-100" src="images/carousel/shadowofthetombraider-carousel.jpg" alt="First slide">
+                      </div>
+                      <div class="carousel-item">
+                          <img class="d-block w-100" src="images/carousel/shadowofthetombraider-carousel.jpg" alt="Second slide">
+                      </div>
+                      <div class="carousel-item">
+                          <img class="d-block w-100" src="images/carousel/shadowofthetombraider-carousel.jpg" alt="Third slide">
+                      </div>
+                </div>
+                <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
                     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                     <span class="sr-only">Previous</span>
-                  </a>
-                  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                </a>
+                <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
                     <span class="carousel-control-next-icon" aria-hidden="true"></span>
                     <span class="sr-only">Next</span>
-                  </a>
-                </div>
+                </a>
             </div>
         </div>
-    </section>
+    </div>   
     
     <br/>
     <br/>
     <br/>
     
+    <div class="container">
     <section>
     
         <?php
@@ -104,7 +118,7 @@
             mysqli_free_result($result);
         ?>
     </section>
-
+    </div>
     <br/>
     <br/>
     <br/>
